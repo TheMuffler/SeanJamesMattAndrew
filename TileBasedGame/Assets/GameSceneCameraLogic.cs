9ffde26194a7b7091d4a639ef95c5d26000d4a3d
Sean King <the0muffler@gmail.com>
2015-11-28 20:57:34 -0800

    Added rotation, increased move speed

--- TileBasedGame/Assets/GameSceneCameraLogic.cs
+++ TileBasedGame/Assets/GameSceneCameraLogic.cs
@@ -1,29 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameSceneCameraLogic : MonoBehaviour {
+public class GameSceneCameraLogic : MonoBehaviour
+{
 
-	public Transform focused = null;
-	public float camSpeed = 4;
+    public Transform focused = null;
+    public float camSpeed = 4;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		float h = Input.GetAxis ("Horizontal");
-		float v = Input.GetAxis ("Vertical");
+    public float camTorque = 180;
 
-		if (Mathf.Abs(h) > 0.1f || Mathf.Abs(v) > 0.1f) {
-			focused = null;
-			transform.position += Vector3.right*h*Time.fixedDeltaTime*camSpeed;
-			transform.position += Vector3.forward*v*Time.fixedDeltaTime*camSpeed;
-		}
+    // Use this for initialization
+    void Start()
+    {
 
-		if (focused != null) {
-			transform.position = Vector3.Lerp(transform.position,focused.position-transform.forward*10,Time.fixedDeltaTime*camSpeed);
-		}
-	}
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(h) > 0.1f || Mathf.Abs(v) > 0.1f)
+        {
+            Vector3 forward = transform.forward;
+            forward.y = 0;
+            forward.Normalize();
+
+            Vector3 right = Vector3.Cross(Vector3.up,forward);
+
+            focused = null;
+            transform.position += right * h * Time.fixedDeltaTime * camSpeed;
+            transform.position += forward * v * Time.fixedDeltaTime * camSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Vector3 pos = transform.position + transform.forward * 10;
+            //transform.Rotate(Vector3.up * Time.fixedDeltaTime * camTorque);
+            transform.Rotate(Vector3.up * Time.fixedDeltaTime * camTorque,Space.World);
+            transform.position = pos - transform.forward * 10;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            Vector3 pos = transform.position + transform.forward * 10;
+            transform.Rotate(Vector3.up * Time.fixedDeltaTime * (-camTorque), Space.World);
+            transform.position = pos - transform.forward * 10;
+        }
+
+        if (focused != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, focused.position - transform.forward * 10, Time.fixedDeltaTime * camSpeed);
+        }
+    }
 }
--- TileBasedGame/Assets/Tasks/Task_MoveToTile.cs
+++ TileBasedGame/Assets/Tasks/Task_MoveToTile.cs
@@ -31,7 +31,7 @@ public class Task_MoveToTile : Task
         Vector3 delta = path[0].TopPosition - unit.transform.position + Vector3.up;
         delta.y = 0;
         unit.transform.rotation = Quaternion.Slerp(unit.transform.rotation,Quaternion.LookRotation(delta),Time.deltaTime*10);
-        if (delta.magnitude < 0.01)
+        if (delta.magnitude < 0.03f)
         {
             unit.transform.position = path[0].TopPosition;
             path.RemoveAt(0);
@@ -47,7 +47,7 @@ public class Task_MoveToTile : Task
         }
         else
         {
-            unit.transform.position += delta.normalized * Mathf.Clamp(0f,Time.deltaTime,delta.magnitude);
+            unit.transform.position += delta.normalized * Mathf.Clamp(0f,Time.deltaTime*1.8f,delta.magnitude);
         }
 
         return false;
