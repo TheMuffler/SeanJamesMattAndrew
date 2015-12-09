ce0d5dd326fcec1067c673a100c4b1a293c1dd38
Sean King <the0muffler@gmail.com>
2015-12-08 18:42:17 -0800

    Fixed stuff

---
+++ TileBasedGame/Assets/GoToAfterSeconds.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToAfterSeconds : MonoBehaviour {
+
+    public float time;
+    public int destination;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(gototask(time));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator gototask(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Application.LoadLevel(destination);
+        yield break;
+    }
+}
